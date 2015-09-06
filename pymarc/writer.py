780ff5ae2952f43ec246d01e7c91b64a831de3a7
Jim Nicholls <jim.nicholls@gmail.com>
2015-09-06 14:48:06 +1000

    Implement XMLWriter to write files of records as a MARCXML collection.

--- pymarc/writer.py
+++ pymarc/writer.py
@@ -1,4 +1,10 @@
+import pymarc
 from pymarc import Record, WriteNeedsRecord
+
+try:
+    import xml.etree.ElementTree as ET  # builtin in Python 2.5
+except ImportError:
+    import elementtree.ElementTree as ET
 
 
 class Writer(object):
@@ -113,6 +119,68 @@ class TextWriter(Writer):
 
         If own_file_handle is True, also closes the file handle.
         """
+        if self.own_file_handle:
+            self.file_handle.close()
+        self.file_handle = None
+
+
+class XMLWriter(Writer):
+    """
+    A class for writing records as a MARCXML collection.
+
+    IMPORTANT: You must the close an XMLWriter, otherwise you will not get
+    a valid XML document.
+
+    Simple usage:
+
+        from pymarc import XMLWriter
+
+        ## pass in a file
+        writer = XMLWriter(open('file.xml','wb'))
+        writer.write(record)
+        writer.close()
+
+        ## use StringIO for Python 2 and BytesIO for Python 3
+        ## if you want to write to a string/bytes.
+        string = StringIO()   ## BytesIO() for Python 3
+        writer = MARCWriter(string, own_file_handle = False)
+        writer.write(record)
+        writer.close()
+        print string
+    """
+
+    def __init__(self, file_handle, own_file_handle = True):
+        """
+        You need to pass in a binary file like object.
+
+        If own_file_handle is True (the default) then the file handle will be
+        closed when the writer is closed. Otherwise the file handle will be
+        left open.
+        """
+        super(XMLWriter, self).__init__()
+        self.file_handle = file_handle
+        self.own_file_handle = own_file_handle
+        self.file_handle.write(
+            b'<?xml version="1.0" encoding="UTF-8"?>')
+        self.file_handle.write(
+            b'<collection xmlns="http://www.loc.gov/MARC21/slim">')
+
+    def write(self, record):
+        """
+        Writes a record.
+        """
+        if not isinstance(record, Record):
+            raise WriteNeedsRecord
+        node = pymarc.record_to_xml_node(record)
+        self.file_handle.write(ET.tostring(node, encoding='utf-8'))
+
+    def close(self):
+        """
+        Closes the file.
+
+        If own_file_handle is True, also closes the file handle.
+        """
+        self.file_handle.write(b'</collection>')
         if self.own_file_handle:
             self.file_handle.close()
         self.file_handle = None
--- test/writer.py
+++ test/writer.py
@@ -2,7 +2,7 @@ import unittest
 import pymarc
 import os
 import textwrap
-from six import BytesIO, StringIO, u
+from six import BytesIO, StringIO, u, binary_type
 
 
 class MARCWriterTest(unittest.TestCase):
@@ -207,10 +207,199 @@ class TextWriterTest(unittest.TestCase):
             'The file handle should NOT close when the writer closes')
 
 
+class XMLWriterTest(unittest.TestCase):
+
+    def test_writing_0_records(self):
+        expected = r"""
+            <?xml version="1.0" encoding="UTF-8"?>
+            <collection xmlns="http://www.loc.gov/MARC21/slim">
+            </collection>
+        """
+        expected = textwrap.dedent(expected[1:]).replace('\n', '')
+        if str != binary_type:
+            expected = expected.encode()
+        file_handle = BytesIO()
+        try:
+            writer = pymarc.XMLWriter(file_handle, own_file_handle = False)
+            writer.close()
+            self.assertEquals(file_handle.getvalue(), expected)
+        finally:
+            file_handle.close()
+
+    def test_writing_empty_record(self):
+        expected = r"""
+            <?xml version="1.0" encoding="UTF-8"?>
+            <collection xmlns="http://www.loc.gov/MARC21/slim">
+            <record>
+            <leader>          22        4500</leader>
+            </record>
+            </collection>
+        """
+        expected = textwrap.dedent(expected[1:]).replace('\n', '')
+        if str != binary_type:
+            expected = expected.encode()
+        file_handle = BytesIO()
+        try:
+            writer = pymarc.XMLWriter(file_handle, own_file_handle = False)
+            record = pymarc.Record()
+            writer.write(record)
+            writer.close()
+            self.assertEquals(file_handle.getvalue(), expected)
+        finally:
+            file_handle.close()
+
+    def test_writing_1_record(self):
+        expected = r"""
+            <?xml version="1.0" encoding="UTF-8"?>
+            <collection xmlns="http://www.loc.gov/MARC21/slim">
+            <record>
+            <leader>          22        4500</leader>
+            <datafield ind1="0" ind2="0" tag="100">
+            <subfield code="a">me</subfield>
+            </datafield>
+            <datafield ind1="0" ind2="0" tag="245">
+            <subfield code="a">Foo /</subfield>
+            <subfield code="c">by me.</subfield>
+            </datafield>
+            </record>
+            </collection>
+        """
+        expected = textwrap.dedent(expected[1:]).replace('\n', '')
+        if str != binary_type:
+            expected = expected.encode()
+        file_handle = BytesIO()
+        try:
+            writer = pymarc.XMLWriter(file_handle, own_file_handle = False)
+            record = pymarc.Record()
+            record.add_field(
+                pymarc.Field('100', ['0', '0'], ['a', u('me')]))
+            record.add_field(
+                pymarc.Field(
+                    '245',
+                    ['0', '0'],
+                    ['a', u('Foo /'), 'c', u('by me.')]))
+            writer.write(record)
+            writer.close()
+            self.assertEquals(file_handle.getvalue(), expected)
+        finally:
+            file_handle.close()
+
+    def test_writing_3_records(self):
+        expected = r"""
+            <?xml version="1.0" encoding="UTF-8"?>
+            <collection xmlns="http://www.loc.gov/MARC21/slim">
+            <record>
+            <leader>          22        4500</leader>
+            <controlfield tag="008">090227s2009    mau                 chi d</controlfield>
+            <datafield ind1="0" ind2="0" tag="100">
+            <subfield code="a">me</subfield>
+            </datafield>
+            <datafield ind1="0" ind2="0" tag="245">
+            <subfield code="a">Foo /</subfield>
+            <subfield code="c">by me.</subfield>
+            </datafield>
+            </record>
+            <record>
+            <leader>          22        4500</leader>
+            <datafield ind1="0" ind2="0" tag="100">
+            <subfield code="a">me</subfield>
+            </datafield>
+            <datafield ind1="0" ind2="0" tag="245">
+            <subfield code="a">Foo /</subfield>
+            <subfield code="c">by me.</subfield>
+            </datafield>
+            </record>
+            <record>
+            <leader>          22        4500</leader>
+            <datafield ind1="0" ind2="0" tag="245">
+            <subfield code="a">Foo /</subfield>
+            <subfield code="c">by me.</subfield>
+            </datafield>
+            </record>
+            </collection>
+        """
+        expected = textwrap.dedent(expected[1:]).replace('\n', '')
+        if str != binary_type:
+            expected = expected.encode()
+        file_handle = BytesIO()
+        try:
+            writer = pymarc.XMLWriter(file_handle, own_file_handle = False)
+            record = pymarc.Record()
+            record.add_field(
+                pymarc.Field(
+                    '008',
+                    data=u('090227s2009    mau                 chi d')))
+            record.add_field(
+                pymarc.Field('100', ['0', '0'], ['a', u('me')]))
+            record.add_field(
+                pymarc.Field(
+                    '245',
+                    ['0', '0'],
+                    ['a', u('Foo /'), 'c', u('by me.')]))
+            writer.write(record)
+            record = pymarc.Record()
+            record.add_field(
+                pymarc.Field('100', ['0', '0'], ['a', u('me')]))
+            record.add_field(
+                pymarc.Field(
+                    '245',
+                    ['0', '0'],
+                    ['a', u('Foo /'), 'c', u('by me.')]))
+            writer.write(record)
+            record = pymarc.Record()
+            record.add_field(
+                pymarc.Field(
+                    '245',
+                    ['0', '0'],
+                    ['a', u('Foo /'), 'c', u('by me.')]))
+            writer.write(record)
+            writer.close()
+            self.assertEquals(file_handle.getvalue(), expected)
+        finally:
+            file_handle.close()
+
+    def test_own_file_handle_true(self):
+        """
+        If a XMLWriter is created with own_file_handle = True, then when the
+        XMLWriter is closed the file handle is also closed.
+        """
+        file_handle = BytesIO()
+        self.assertFalse(
+            file_handle.closed,
+            'The file handle should be open')
+        writer = pymarc.XMLWriter(file_handle, own_file_handle = True)
+        self.assertFalse(
+            file_handle.closed,
+            'The file handle should still be open')
+        writer.close()
+        self.assertTrue(
+            file_handle.closed,
+            'The file handle should close when the writer closes')
+
+    def test_own_file_handle_false(self):
+        """
+        If a XMLWriter is created with own_file_handle = False, then when the
+        XMLWriter is closed the file handle is NOT also closed.
+        """
+        file_handle = BytesIO()
+        self.assertFalse(
+            file_handle.closed,
+            'The file handle should be open')
+        writer = pymarc.XMLWriter(file_handle, own_file_handle = False)
+        self.assertFalse(
+            file_handle.closed,
+            'The file handle should still be open')
+        writer.close()
+        self.assertFalse(
+            file_handle.closed,
+            'The file handle should NOT close when the writer closes')
+
+
 def suite():
     marc_suite = unittest.makeSuite(MARCWriterTest, 'test')
     text_suite = unittest.makeSuite(TextWriterTest, 'test')
-    test_suite = unittest.TestSuite((marc_suite, text_suite))
+    xml_suite = unittest.makeSuite(XMLWriterTest, 'test')
+    test_suite = unittest.TestSuite((marc_suite, text_suite, xml_suite))
     return test_suite
 
 if __name__ == '__main__':
