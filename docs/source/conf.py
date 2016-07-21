016b240e95b833217fdcaca0335f0b23f3e075f6
Geoffrey Spear <speargh@pitt.edu>
2016-07-21 12:56:26 -0400

    fix sys.path in sphinx config so it can find the modules to document

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,10 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-# import sys
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+print(sys.path)
 # sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
