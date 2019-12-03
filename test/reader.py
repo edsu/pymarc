7d0ff6c9d3e4ab232afd08c30dffd5bed76c3814
Pierre Verkest <pverkest@anybox.fr>
2019-12-03 21:39:33 +0100

    avoid running same test twice
    as MARCReaderStringTest was MARCReaderFileTest child some test dedicated to
    MARCReaderFileTest was running in MARCReaderStringTest test class with no additionnal values
    as they were not using self.reader

--- test/reader.py
+++ test/reader.py
@@ -6,7 +6,26 @@ import pymarc
 
 from six.moves.urllib.request import urlopen
 
-class MARCReaderFileTest(unittest.TestCase):
+class MARCReaderBaseTest(object):
+
+    def test_iterator(self):
+        count = 0
+        for record in self.reader:
+            count += 1
+        self.assertEqual(count, 10,
+                'found expected number of MARC21 records')
+
+    def test_string(self):
+        ## basic test of stringification
+        starts_with_leader = re.compile('^=LDR')
+        has_numeric_tag = re.compile('\n=\d\d\d ')
+        for record in self.reader:
+            text = str(record)
+            self.assertTrue(starts_with_leader.search(text), 'got leader')
+            self.assertTrue(has_numeric_tag.search(text), 'got a tag')
+
+
+class MARCReaderFileTest(unittest.TestCase, MARCReaderBaseTest):
     """
     Tests for the pymarc.MARCReader class which provides iterator
     based access to a MARC file.
@@ -18,13 +37,6 @@ class MARCReaderFileTest(unittest.TestCase):
     def tearDown(self):
         if self.reader:
             self.reader.close()
-
-    def test_iterator(self):
-        count = 0
-        for record in self.reader:
-            count += 1
-        self.assertEqual(count, 10,
-                'found expected number of MARC21 records')
 
     def test_map_records(self):
         self.count = 0
@@ -44,15 +56,6 @@ class MARCReaderFileTest(unittest.TestCase):
         self.assertEqual(self.count, 20, 'map_records appears to work')
         fh1.close()
         fh2.close()
-
-    def test_string(self):
-        ## basic test of stringification
-        starts_with_leader = re.compile('^=LDR')
-        has_numeric_tag = re.compile('\n=\d\d\d ')
-        for record in self.reader:
-            text = str(record)
-            self.assertTrue(starts_with_leader.search(text), 'got leader')
-            self.assertTrue(has_numeric_tag.search(text), 'got a tag')
 
     def disabled_test_codecs(self):
         import codecs
@@ -83,7 +86,8 @@ class MARCReaderFileTest(unittest.TestCase):
             self.assertEqual(record['752']['d'], 'Kostroma')
 
 
-class MARCReaderStringTest(MARCReaderFileTest):
+
+class MARCReaderStringTest(unittest.TestCase, MARCReaderBaseTest):
 
     def setUp(self):
         fh = open('test/test.dat')
