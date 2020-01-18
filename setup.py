e287716f8253d33f81e7e795379c3f07aba8fade
Mikhail Terekhov <termim@gmail.com>
2020-01-18 18:00:29 -0500

    drop python < 3.6

--- setup.py
+++ setup.py
@@ -9,16 +9,10 @@ Intended Audience :: Education
 Intended Audience :: Developers
 Intended Audience :: Information Technology
 License :: OSI Approved :: BSD License
-Programming Language :: Python
-Programming Language :: Python :: 2
-Programming Language :: Python :: 2.6
-Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.3
-Programming Language :: Python :: 3.4
-Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
 Topic :: Text Processing :: General
 """
 
@@ -39,5 +33,5 @@ setup(
     long_description_content_type="text/markdown",
     classifiers=list(filter(None, classifiers.split("\n"))),
     test_suite="test",
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*",
+    python_requires=">=3.6.*",
 )
